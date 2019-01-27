0887608d47e7acce21995187ab44833dc3bbabed
richard.supak Supak <richard.supak@icloud.com>
2019-01-27 02:43:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state
+board = Array.new(9) { ' ' }
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts '-' * 11
